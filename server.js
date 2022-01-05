48b16fb24ea68890a83fe37300e1a2edbfc78a02
ravi3276 <ravitejareddy264@gmail.com>
2022-01-05 16:31:57 +0530

    final submition

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-import express from 'express';
-import dotenv from "dotenv";
-import {OAuth2Client} from "google-auth-library"
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const { OAuth2Client } = require('google-auth-library');
 
 dotenv.config();
+const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
 const app = express();
+app.use(express.json());
 
-app.use(express.json())
-
-const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_API_KEY);
 
 app.post('/api/google-login', async (req, res) => {
-    const { token } = req.body;
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: process.env.CLIENT_ID,
-    });
-    const { name, email, picture } = ticket.getPayload();
-    res.status(201);
-    res.json({ name, email, picture });
+  const { token } = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
   });
+  const { name, email, picture } = ticket.getPayload();
+  res.status(201);
+  res.json({ name, email, picture });
+});
 
-app.listen(process.env.PORT || 5000 ,()=>{
-    console.log("server listing to port")
+app.use(express.static(path.join(__dirname, '/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/build/index.html'))
+);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    `Server is ready at http://localhost:${process.env.PORT || 5000}`
+  );
 });
